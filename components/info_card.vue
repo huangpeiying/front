086f92b4b1b0de96e383d9c71f83c2b93051032c
huangpeiying <969433397@qq.com>
2019-07-19 17:34:28 +0800

    first backup

---
+++ components/info_card.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="info_card">
+      <div class="second_framework" :style="{ 'background-color': c_par }">
+        <div class="English_Name" :style="{ 'color':d_par}">{{English_Name}}</div>
+        <div class="part1">
+        <div class="Chinese_name">{{Chinese_name}}</div>
+        <div class="number">{{number}}</div>
+        </div>
+        <div class="part2">
+          <div class="normal">
+            <div class="Nor">正常</div>
+            <div class="Num1">18</div>
+            <div class="Abnormal">
+              <div class="abnormal">异常</div>
+              <div class="Num2">2</div>
+            </div>
+            <div class="Serious">
+              <div class="serious">严重</div>
+              <div class="Num3">1</div>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'InfoCard',
+    props: [
+      'English_Name',
+      'c_par',
+      'Chinese_name',
+      'number',
+      'd_par'
+    ]
+  }
+</script>
+
+
+<style scoped>
+.second_framework{
+  display: flex;
+  flex-direction: column;
+  height: 164px;
+  width: 315px;
+  background-color: #0DB983;
+  margin-left: 30px;
+  border-radius:10px;
+}
+.English_Name{
+  margin-top:60px ;
+  margin-left: 20px;
+  /*color: rgb(127,208,207);*/
+  font-size: 12px;
+
+}
+.part1{
+  display: flex;
+  flex-direction: row;
+}
+
+
+.Chinese_name{
+   margin-top:8px ;
+   margin-left: 20px;
+   color: #FFFFFF;
+   font-size: 20px;
+  font-weight: bold;
+ }
+  .number{
+    margin-top:8px ;
+    margin-left: 40px;
+    color: #FFFFFF;
+    font-size: 20px;
+    font-weight: bold;
+  }
+.part2{
+  display: flex;
+  flex-direction: row;
+  /*align-content: center;*/
+  margin-top: 18px;
+  width:315px;
+  height:44px;
+  background:rgb(255, 255, 255);
+  z-index: 1;
+  border-radius:0 0 10px 10px;
+
+}
+.normal{
+  display: flex;
+  flex-direction: row;
+  margin-top: 10px;
+
+}
+
+.Nor{
+  font-size: 14px;
+  margin-left: 20px;
+  color: rgb(85,97,117);
+ }
+
+
+.Num1{
+  font-size: 14px;
+  margin-left: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: #0DB983;
+}
+.Abnormal{
+  display: flex;
+  flex-direction: row;
+  margin-left: 15px;
+  width:105px;
+  height:44px;
+  border-style:none solid none solid;
+  border-color:rgb(239, 239, 240);
+  border-width: 1px 1px
+}
+  .abnormal{
+    font-size: 14px;
+    margin-left: 20px;
+    color: rgb(85,97,117);
+  }
+  .Num2{
+    font-size: 14px;
+    margin-left: 30px;
+    text-align: center;
+    font-weight: bold;
+    color: #ED7346;
+  }
+
+.Serious{
+  display: flex;
+  flex-direction: row;
+}
+.serious{
+  font-size: 14px;
+  margin-left: 20px;
+  color: rgb(85,97,117);
+}
+.Num3{
+  font-size: 14px;
+  margin-left: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: #ED4646;
+}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="General_framework">
+    <div class="Project_name">{{company_name}}</div>
+    <div class="Sloth_Technology">{{Sloth_Technology}}</div>
+    <div class="info_card_list">
+      <div class="green">
+      <InfoCard  c_par="#0DB983" d_par="rgb(127,208,207)"English_Name="Air condition" Chinese_name="空调" number="21"></InfoCard>
+      </div>
+      <div class="yellow">
+        <InfoCard c_par="#FFAF27" d_par="rgb(223,216,168)" English_Name="Elevator" Chinese_name="电梯" number="21"></InfoCard>
+      </div>
+      <div class="black">
+        <InfoCard c_par="#000022"  d_par="rgb(223,216,168)" English_Name="Elevator" Chinese_name="电梯" number="21"></InfoCard>
+      </div>
+      <div class="red">
+        <InfoCard c_par="#ED4646"  d_par="rgb(223,216,168)" English_Name="Elevator" Chinese_name="电梯" number="21"></InfoCard>
+      </div>
+    </div>
+    <div class="three_pic">
+      <div class="first_pic">
+      <img class="set_up" src="../../../static/images/Set_up.png" alt="设置" >
+      </div>
+      <div class="second_pic">
+        <img class="service" src="../../../static/images/service.png" alt="客服">
+      </div>
+
+      <div class="third_pic">
+        <img class="Personal" src="../../../static/images/Personal.png" alt="个人用户">
+      </div>
+
+
+    </div>
+    </div>
+<!--    <button v-on:click="bt" v-bind:class ="{'c1': isActive, 'c2': hasError }">button</button>-->
+
+
+</template>
+
+<script>
+  import InfoCard from '../../components/info_card'
+  export default {
+    data () {
+      return {
+        company_name: '树懒科技',
+        Sloth_Technology: 'Sloth Technology Co., Ltd.',
+        hasError: true,
+        isActive: false
+
+      }
+    },
+    components: {
+      InfoCard
+    },
+    methods: {
+      bt () {
+        if (this.isActive === false) {
+          this.isActive = true
+          this.hasError = false
+        } else {
+          this.isActive = false
+          this.hasError = true
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  page{
+    background: rgb(239,239,240);
+    height: 100%;
+  }
+
+</style>
+
+
+<style scoped>
+
+  .General_framework{
+    display: flex;
+    flex-direction: column;
+    height:1000px;
+    width: auto;
+
+
+  }
+  .Project_name{
+    /*color: rgb(116,128,149);*/
+    font-size: 24px;
+    margin-left: 30px;
+    margin-top: 20px;
+    font-weight: bold;
+
+  }
+.Sloth_Technology{
+  color: rgb(116,128,149);
+  font-size: 12px;
+  margin-left: 30px;
+  margin-top: 14px;
+}
+.green{
+  margin-top: 77px;
+}
+.yellow{
+  margin-top: 20px;
+
+}
+  .c1{
+    background-color: blueviolet;
+  }
+  .c2{
+    background-color: red;
+  }
+.three_pic{
+  display: flex;
+  flex-direction:row;
+  position: fixed;
+  height: 78px;
+  /*width: 96px;*/
+  background-color: rgb(239,239,240);
+  bottom:0;
+  z-index: 999;
+
+}
+.first_pic{
+  height: 48px;
+  width: 96px;
+  background-color: #FFFFFF;
+  /*margin-top: 5px;*/
+  margin-left: 30px;
+  border-radius:4px;
+
+}
+  .set_up{
+    margin-left: 39px;
+    margin-top: 15px;
+    width: 18px;
+    height: 18px;
+
+  }
+  .second_pic{
+    height: 48px;
+    width: 96px;
+    background-color: #FFFFFF;
+    /*margin-top: 5px;*/
+    margin-left: 14px;
+    border-radius:4px;
+  }
+.service{
+  margin-left: 39px;
+  margin-top: 15px;
+  width: 18px;
+  height: 18px;
+}
+.third_pic{
+  height: 48px;
+  width: 96px;
+  background-color: #FFFFFF;
+  /*margin-top: 5px;*/
+  margin-left: 14px;
+  border-radius:4px;
+}
+.Personal{
+  margin-left: 39px;
+  margin-top: 15px;
+  width: 18px;
+  height: 18px;
+}
+  .black{
+    margin-top: 20px;
+  }
+.red{
+  margin-top: 20px;
+}
+</style>
